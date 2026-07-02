a3f70fac08e55354a3c13384c47193657870d2ed
Sanket2329 <sanketshakya62@gmail.com>
2026-07-02 16:30:14 +0530

    Lazy load SentenceTransformer

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -4,17 +4,17 @@ class EmbeddingService:
     @classmethod
     def get_model(cls):
         if cls._model is None:
-            print("Importing SentenceTransformer...", flush=True)
+            print("STEP 1", flush=True)
 
             from sentence_transformers import SentenceTransformer
 
-            print("Loading model...", flush=True)
+            print("STEP 2", flush=True)
 
             cls._model = SentenceTransformer("models/all-MiniLM-L6-v2")
 
-            print("Model loaded.", flush=True)
+            print("STEP 3", flush=True)
 
         return cls._model
 
-    def embed(self, text: str):
+    def embed(self, text):
         return self.get_model().encode(text).tolist()
